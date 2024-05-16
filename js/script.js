1029393ad3d55d3c7022dcbf4733ad75c53c0da1
Nazik5436 <166996612+Nazik5436@users.noreply.github.com>
2024-05-16 19:19:51 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const icon = document.querySelector('.icon')
+const logo_img = document.querySelector('.logo_img')
+
+icon.addEventListener("click", function(e) {
+    logo_img.style.display = 'block'
+})
