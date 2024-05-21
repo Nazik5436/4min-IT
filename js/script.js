5b356284a42216c038d16c5203a91cfd57b7dcf4
Nazik5436 <166996612+Nazik5436@users.noreply.github.com>
2024-05-21 20:52:59 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
+ const close = document.querySelector('.close')
  const burger = document.querySelector('.burger')
  const random_block = document.querySelector('.random_block')
  const nav_links_1 = document.querySelector('.nav_links_1')
  const nav_links_2 = document.querySelector('.nav_links_2')
  const nav_links_3 = document.querySelector('.nav_links_3')
  const nav_links_4 = document.querySelector('.nav_links_4')
+
+ close.addEventListener('click', function (e) {
+   random_block.style.display = 'none'
+})
 
  burger.addEventListener('click', function (e) {
     random_block.style.display = 'flex'
