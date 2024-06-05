016e7677ea04e0e740887648ef453e6434529157
Nazik5436 <166996612+Nazik5436@users.noreply.github.com>
2024-06-05 17:14:21 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,60 @@
+
+
+// alert('Hello world')
+
+console.log('Hello world')
+
+                              //зміні
+
+// let a = 1
+// console.log(a)
+
+// a = 2
+// console.log(a)
+
+
+// let a = 'Hy world'
+
+//pes Patron
+
+// let pesPatron
+
+//////////////////
+
+// let age
+// age = prompt('Введіть свій вік')
+
+// alert(age)
+
+
+               ///№1
+
+// let name = 'andrii'
+// let surname = 'smolinskyi'
+// console.log(name, surname)
+
+// //////Д/з
+
+// let pr
+// pr = prompt('Теперішній президент України ?')
+// alert(pr)
+
+// let president = 'Володимир'
+// console.log(president)
+
+
+               ///№2
+
+// let name = 'Іван'
+// let admin = name
+// console.log(name) 
+
+
+
+///типи даних
+
+//числовий 
+
+
+
+
