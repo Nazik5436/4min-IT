3a704ae49a916f6bc9426ba21872ac36bbe9eb37
Nazik5436 <166996612+Nazik5436@users.noreply.github.com>
2024-06-21 17:34:29 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,90 @@
+// виведення парних чисел за допомогою циклів
+
+// for (let index = 0; index < 10; index++) {
+//     if (index % 2 == 0 && index != 0) {
+//         console.log(index) 
+//     }
+// }
+
+
+
+
+// таблиця множення для числа 5 
+
+// let a = 1;
+
+// while (a < 11){
+//     console.log(a*5)
+//     a++
+// }
+
+
+
+
+
+// Числа від 1 до 10
+
+// for (let index = 1; index < 11; index++) {
+//         console.log(index)  
+// }
+
+
+
+
+//факторіал числа 6
+
+// let result = 1
+// let count = 7 
+
+// for (let index = 1; index < count; index++) {
+//     result = result*index
+// }
+
+// console.log(result) 
+
+
+
+
+
+// рядок з 5 *
+
+// let star = ""
+
+// for (let index = 0; index < 5; index++) {
+//     star += "*"
+//     console.log(star)
+    
+// }
+
+
+
+
+// сума чисел в діапазоні 
+
+// let a = 2
+// let b = 10 
+// let result = 0
+
+// for (let index = a; index < b+1; index++) {
+//     result = result + index
+    
+// }
+
+// console.log(result) 
+
+
+
+
+
+// сума чисел в діапазоні prompt
+
+// let a = Number(prompt(`Введіть 1 число діапазону`))
+// let b = Number(prompt(`Введіть 2 число діапазону`)) 
+// let result = 0
+
+// for (let index = a; index < b+1; index++) {
+//      result = result + index
+    
+// }
+
+// console.log(result) 
