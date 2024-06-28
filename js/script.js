625b5fccb64b45689aee8353c4744bbca710663a
Nazik5436 <166996612+Nazik5436@users.noreply.github.com>
2024-06-28 16:40:43 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// Виведення меншого числа 
+// function min(a, b){
+//     let min = 0 
+//     if (a > b){
+//         min = b 
+        
+//     }else if(a < b){
+//         min = a 
+//     }else{
+//         console.log(`Числа рівні`)
+//     }
+//     return min 
+// }
+
+// let count = min(3, 5)
+// console.log(count)
+
+
+
+
+// Перевірка на парність
+
+// function parnist(a){
+//     let result
+//     if (a % 2 == 0){
+//         result = true 
+//     }else{
+//         result = false
+//     }
+//     return result
+// }
+
+// console.log(parnist(1))
+
+
+
+
+//  Функція square яка приймає число і повертає його в квадрат
+
+// function square (a){
+//     return a*a
+// }
+
+// console.log(square(5))
+
+
+
+// Обчислення факторіалу 
