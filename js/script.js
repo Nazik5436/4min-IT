0648651804b80242a6da55c78ba7d40fd5ec8ae6
Nazik5436 <166996612+Nazik5436@users.noreply.github.com>
2024-07-23 11:07:37 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-$(".multiple-items").slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 3,
-  slidesToScroll: 3
-});
+$(".single-item").slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1
+  });
 
   
