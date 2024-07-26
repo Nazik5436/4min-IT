a78d25d7a3c7e133ab2fc290fec738e6433c260e
Nazik5436 <166996612+Nazik5436@users.noreply.github.com>
2024-07-26 12:40:36 +0300

    1

--- js/script.js
+++ js/script.js
@@ -24,29 +24,101 @@ const API = 'http://www.omdbapi.com/?i=tt3896198&apikey=fc0a8f0c'
 //     .catch(error => console.error(error)) 
 
 
-const SetRequest = async () =>{
-    try {
-        const response = await fetch(API)
-        const data = await response.json() //розпарсюєм
-        console.log(data)
-        // створення картки
-        const container = document.querySelector('.posts')
-        let htmlContent = ''
-        data.forEach(element => {
-            htmlContent += `
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">${element.Title}</h5>
-                        <p class="card-text">${element.body}</p>
-                    </div>
-                </div>
-            `
 
-            container.innerHTML = htmlContent
-        });
-    } catch (error) {
+
+
+// const SetRequest = async () =>{
+//     try {
+//         const response = await fetch(API)
+//         const data = await response.json() //розпарсюєм
+//         console.log(data)
+//         // створення картки
+//         const container = document.querySelector('.posts')
+//         let htmlContent = ''
+//         data.Object.forEach(element => {
+//             htmlContent += `
+//                 <div class="card" style="width: 18rem;">
+//                     <div class="card-body">
+//                         <img src="${element.Poster}" class="card-img-top" alt="...">
+//                         <h5 class="card-title">${element.Title}</h5>
+//                         <p class="card-text">${element.body}</p>
+//                     </div>
+//                 </div>
+//             `
+
+//             container.innerHTML = htmlContent
+//         });
+//     } catch (error) {
+//         console.error('error', error)
         
-    }
+//     }
     
-} 
-SetRequest()
+// } 
+// SetRequest()
+
+
+
+
+
+// Андрюха
+
+
+// const SetRequest = async () =>{
+//     try {
+//       const response = await fetch(API)
+  
+//       const data = await response.json()
+//       console.log(data)
+//       // створення карточки
+//       const container = document.querySelector('.posts')
+//       let htmlContent =''
+  
+//       data.Search.forEach(element => {
+//         htmlContent += 
+//           <div class="card" style="width: 18rem;">
+//             <div class="card-body">
+//               <img src="${element.Poster}" class="card-img-top" alt="...">
+//               <h5 class="card-title">${element.Title}</h5>
+//               <p class="card-text">${element.Year}</p>
+//             </div>
+//           </div>
+        
+//         container.innerHTML = htmlContent
+//       });
+  
+//     } catch (error) {
+//       console.error('error', error)
+//     }
+//   }
+  
+//   SetRequest()
+
+
+///////////////////////////////////////////////////////////// перепис
+
+// const SetRequest = async () =>{
+//     try {
+//         const response = await fetch(API)
+//         const data = await response.json()
+//         console.log(data)
+//         const container = document.querySelector('.posts')
+//         let htmlContent = ''
+//         data.forEach(element => {
+//             htmlContent += `
+//                 <div class="card" style="width: 18rem;">
+//                     <div class="card-body">
+//                         <img src="${element.Poster}" class="card-img-top" alt="Poster">
+//                         <h5 class="card-title">${element.Title}</h5>
+//                         <p class="card-text">${element.body}</p>
+//                     </div>
+//                 </div>
+//             `    
+//             container.innerHTML = htmlContent
+//         });
+//     } catch (error) {
+//         console.error('error', error)
+//     }
+// }
+
+
+
