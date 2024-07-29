9f0c403b48ab17d8e21c5b31ac3ae620a0fa3313
Nazik5436 <166996612+Nazik5436@users.noreply.github.com>
2024-07-29 18:59:50 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-const API = 'http://www.omdbapi.com/?s=Star+wars&apikey=fc0a8f0c'
-const API_starWars = 'http://www.omdbapi.com/?t=Star+wars'
+const API_starWars = 'http://www.omdbapi.com/?s=Star+wars&apikey=fc0a8f0c'
+const API_Avanger = 'http://www.omdbapi.com/?s=Avengers&apikey=fc0a8f0c'
+const API_Lord = 'http://www.omdbapi.com/?s=Lord+of+the+rings&apikey=fc0a8f0c'
+const API_HarryPotter = 'http://www.omdbapi.com/?s=harry+potter&apikey=fc0a8f0c'
+const API_FastAndFurious = 'http://www.omdbapi.com/?s=fast+and+furious&apikey=fc0a8f0c'
+
 
 // fetch(`${API}/posts`)
 //     .then(response => response.json()) //розпарсюєм
@@ -30,21 +34,34 @@ const API_starWars = 'http://www.omdbapi.com/?t=Star+wars'
 
  const SetRequest = async () =>{
      try {
-         const response = await fetch(API)
-         const data = await response.json() 
-         console.log(data)
+        const responseFirst = await fetch(API_starWars)
+        const responseSecond = await fetch(API_Avanger)
+        const responseThird = await fetch(API_Lord)
+        const responseFourth = await fetch(API_HarryPotter)
+        const responseFifth = await fetch(API_FastAndFurious)
+
+
+
+        const dataFirst = await responseFirst.json() 
+        const dataSecond = await responseSecond.json()
+        const dataThird = await responseThird.json()
+        const dataFourth = await responseFourth.json()
+        const dataFifth = await responseFifth.json()
+
+
+        const data = [...dataFirst.Search, ...dataSecond.Search, ...dataThird.Search, ...dataFourth.Search, ...dataFifth.Search]
+        console.log(data)
         
          const container = document.querySelector('.posts')
          let htmlContent = ''
-         data.Search.forEach(element => {
+         data.forEach(element => {
              htmlContent += `
                  <div class="card" style="width: 18rem;">
-                     <div class="card-body">
+                     <div id='StarWars' class="card-body">
                          <img src="${element.Poster}" class="card-img-top" alt="...">
                          <p class="card-text">${element.Type}</p>
                          <h5 class="card-title">${element.Title}</h5>
                          <p class="card-text">${element.Year}</p>
-                         
                      </div>
                  </div>
              `
@@ -58,4 +75,3 @@ const API_starWars = 'http://www.omdbapi.com/?t=Star+wars'
     
  } 
  SetRequest()
-
