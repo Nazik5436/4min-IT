dce01ea0319feb612116e7796cd867a37d3d3a56
Nazik5436 <166996612+Nazik5436@users.noreply.github.com>
2024-07-30 16:29:02 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,15 @@
+localStorage.setItem("name", "Andrii") //запис в локалсторедж
+
+console.log(localStorage.getItem("name")) // отримати
+
+/////////
+
+localStorage.removeItem("name") // full delete
+
+localStorage.clear()
+
+///////// Cookie
+
+document.cookie = "username=John"
+
+
