5ab69c4448d29486240fa297718ecd6809d4aa90
Nazik5436 <166996612+Nazik5436@users.noreply.github.com>
2024-07-30 15:14:44 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,34 +1,53 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const itemsContainer = document.querySelector('.items');
-    const pageButtons = document.querySelectorAll('.page-btn');
-    const itemsPerPage = 10;
-    const totalItems = 441; // Загальна кількість елементів
-  
-    // Функція для генерації елементів
-    function generateItems(page) {
-      itemsContainer.innerHTML = ''; // Очистити контейнер
-      const start = (page - 1) * itemsPerPage;
-      const end = start + itemsPerPage;
-      for (let i = start; i < end && i < totalItems; i++) {
-        const item = document.createElement('div');
-        item.classList.add('item');
-        item.textContent = `Item ${i + 1}`;
-        itemsContainer.appendChild(item);
-      }
-    }
-  
-    // Додати обробник подій для кнопок пагінації
-    pageButtons.forEach(button => {
-      button.addEventListener('click', function() {
-        pageButtons.forEach(btn => btn.classList.remove('active'));
-        this.classList.add('active');
-        const page = parseInt(this.getAttribute('data-page'));
-        generateItems(page);
-      });
+const API_starWars = 'http://www.omdbapi.com/?s=Star+wars&apikey=fc0a8f0c';
+const API_Avanger = 'http://www.omdbapi.com/?s=Avengers&apikey=fc0a8f0c';
+const API_Lord = 'http://www.omdbapi.com/?s=Lord+of+the+rings&apikey=fc0a8f0c';
+const API_HarryPotter = 'http://www.omdbapi.com/?s=harry+potter&apikey=fc0a8f0c';
+const API_SpiderMan = 'http://www.omdbapi.com/?s=spider+man&apikey=fc0a8f0c';
+
+const SetRequest = async () => {
+  try {
+    const responseFirst = await fetch(API_starWars);
+    const responseSecond = await fetch(API_Avanger);
+    const responseThird = await fetch(API_Lord);
+    const responseFourth = await fetch(API_HarryPotter);
+    const responseFifth = await fetch(API_SpiderMan);
+
+    const dataFirst = await responseFirst.json();
+    const dataSecond = await responseSecond.json();
+    const dataThird = await responseThird.json();
+    const dataFourth = await responseFourth.json();
+    const dataFifth = await responseFifth.json();
+
+    const data = [...dataFirst.Search, ...dataSecond.Search, ...dataThird.Search, ...dataFourth.Search, ...dataFifth.Search];
+    console.log(data);
+
+    const container = document.querySelector('.posts');
+    let htmlContent = '';
+    data.forEach(element => {
+      htmlContent += `
+        <div class="card">
+          <div class="card-inner">
+            <div class="card-front">
+              <img src="${element.Poster}" class="card-img-top" alt="${element.Title}">
+              <div class="card-body">
+                <h5 class="card-title">${element.Title}</h5>
+              </div>
+            </div>
+            <div class="card-back">
+              <div>
+                <h5 class="card-title">${element.Title}</h5>
+                <p class="card-text">${element.Type} | ${element.Year}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
     });
-  
-    // Генерація елементів для першої сторінки за замовчуванням
-    generateItems(1);
-    pageButtons[0].classList.add('active');
-  });
-  
+
+    container.innerHTML = htmlContent;
+  } catch (error) {
+    console.error('error', error);
+  }
+}
+
+SetRequest();
