aa9a48a614f3998a030a55ee44587ae0fdebc14b
Nazik5436 <166996612+Nazik5436@users.noreply.github.com>
2024-08-01 12:31:46 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,90 +1,49 @@
-const API_starWars = 'http://www.omdbapi.com/?s=Star+wars&apikey=fc0a8f0c'
-const API_Avanger = 'http://www.omdbapi.com/?s=Avengers&apikey=fc0a8f0c'
-const API_Lord = 'http://www.omdbapi.com/?s=Lord+of+the+rings&apikey=fc0a8f0c'
-const API_HarryPotter = 'http://www.omdbapi.com/?s=harry+potter&apikey=fc0a8f0c'
-const API_SpiderMan = 'http://www.omdbapi.com/?s=spider+man&apikey=fc0a8f0c'
+const API_starWars = 'http://www.omdbapi.com/?s=Star+wars&apikey=fc0a8f0c';
+const API_Avanger = 'http://www.omdbapi.com/?s=Avengers&apikey=fc0a8f0c';
+const API_Lord = 'http://www.omdbapi.com/?s=Lord+of+the+rings&apikey=fc0a8f0c';
+const API_HarryPotter = 'http://www.omdbapi.com/?s=harry+potter&apikey=fc0a8f0c';
+const API_SpiderMan = 'http://www.omdbapi.com/?s=spider+man&apikey=fc0a8f0c';
 
-// fetch(`${API}/posts`)
-//     .then(response => response.json()) //розпарсюєм
-//     .then(response => {
-//         const container = document.querySelector('.posts')
-//         let htmlContent = ''
+const SetRequest = async (api, nameFilm) => {
+    try {
+        const responseFirst = await fetch(api);
+        const dataFirst = await responseFirst.json();
 
-//         response.forEach(post => {
-//             htmlContent += `
-//             <div class="card" style="width: 18rem;">
-//                 <div class="card-body">
-//                     <h5 class="card-title">${post.title}</h5>
-//                     <p class="card-text">${post.body}</p>
-//                 </div>
-//             </div>
-//             `
+        const data = dataFirst.Search;
+        const container = document.querySelector('.posts');
+        let htmlContent = '';
+        data.forEach(element => {
+            if (!document.querySelector(`.card[data-imdbid="${element.imdbID}"]`)) {
+                htmlContent += `
+                    <div class="card" id="${nameFilm}" data-imdbid="${element.imdbID}" style="width: 18rem;">
+                        <div class="card-body">
+                            <img src="${element.Poster}" class="card-img-top" alt="...">
+                            <p class="card-text">${element.Type}</p>
+                            <h5 class="card-title">${element.Title}</h5>
+                            <p class="card-text">${element.Year}</p>
+                        </div>
+                    </div>
+                `;
+            }
+        });
+        container.insertAdjacentHTML('beforeend', htmlContent);
+    } catch (error) {
+        console.error('error', error);
+    }
+};
 
-            
-//         });
+SetRequest(API_starWars, "StarWars");
+SetRequest(API_Avanger, "Avengers");
+SetRequest(API_Lord, "Lord_of_the");
+SetRequest(API_HarryPotter, "HarryPotter");
+SetRequest(API_SpiderMan, "SpiderMan");
 
-//         container.innerHTML = htmlContent
-//     })
-//     .catch(error => console.error(error)) 
-
-
-
-
-
- const SetRequest = async () =>{
-     try {
-        const responseFirst = await fetch(API_starWars)
-        const responseSecond = await fetch(API_Avanger)
-        const responseThird = await fetch(API_Lord)
-        const responseFourth = await fetch(API_HarryPotter)
-        const responseFifth = await fetch(API_SpiderMan)
-
-
-        const dataFirst = await responseFirst.json() 
-        const dataSecond = await responseSecond.json()
-        const dataThird = await responseThird.json()
-        const dataFourth = await responseFourth.json()
-        const dataFifth = await responseFifth.json()
-
-
-        const data = [...dataFirst.Search, ...dataSecond.Search, ...dataThird.Search, ...dataFourth.Search, ...dataFifth.Search]
-        console.log(data)
-        
-         const container = document.querySelector('.posts')
-         let htmlContent = ''
-         data.forEach(element => {
-             htmlContent += `
-                 <div class="card" data-imdbid="${element.imdbID}" style="width: 18rem;">
-                     <div id='StarWars' class="card-body">
-                         <img src="${element.Poster}" class="card-img-top" alt="...">
-                         <p class="card-text">${element.Type}</p>
-                         <h5 class="card-title">${element.Title}</h5>
-                         <p class="card-text">${element.Year}</p>
-                     </div>
-                 </div>
-             `
-
-             container.innerHTML = htmlContent
-
-             
-            
-
-         });
-     } catch (error) {
-         console.error('error', error)
-        
-     }
-    
- } 
- SetRequest()
-
- document.querySelector('.posts').addEventListener('click', async (event) => {
+document.querySelector('.posts').addEventListener('click', async (event) => {
     const card = event.target.closest('.card');
     if (card) {
         const imdbID = card.getAttribute('data-imdbID');
         const details = await fetch(`http://www.omdbapi.com/?i=${imdbID}&apikey=fc0a8f0c`);
         const movieDetails = await details.json();
-        console.log(movieDetails); 
+        console.log(movieDetails);
     }
- });
- 
+});
